0a79184082ffc8f9d2a57fa9854e8f6e66232397
ozeraydin57 <ozeraydin57@gmail.com>
2020-06-11 00:57:51 +0300

    initial commit

---
+++ JWTCoreApi/Controllers/TokenController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Post([FromBody] LoginRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _userService(request.UserName, request.Password);
+                if (user == null)
+                {
+                    return Unauthorized();
+                }
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, request.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                var token = new JwtSecurityToken
+                (
+                    issuer: _configuration["Issuer"], //appsettings.json içerisinde bulunan issuer değeri
+                    audience: _configuration["Audience"],//appsettings.json içerisinde bulunan audince değeri
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddDays(30), // 30 gün geçerli olacak
+                    notBefore: DateTime.UtcNow,
+                    signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SigningKey"])),//appsettings.json içerisinde bulunan signingkey değeri
+                            SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+            return BadRequest();
+        }
+
+
+        private User _userService(string userName, string password)
+        {
+            User user = null;
+
+            if (users.Where(k => k.UserName == userName && k.Password == password).Any()) //kayıt varsa
+            {
+                user = new User
+                {
+                    UserName = userName,
+                    Password = password
+                };
+            }
+
+            return user;
+        }
+        List<User> users = new List<User>()
+        {
+             new User{ UserName="Özer", Password="123"},
+             new User{UserName="test",Password="test"}
+        };
+    }
+
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ JWTCoreApi/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTCoreApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("api/[controller]/usersecurelist")]
+        public IEnumerable<string> SecureList()
+        {
+            List<string> list = new List<string>()
+            {
+                "Özer","Hasan","Kamil"
+            };
+            return list;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/[controller]/userpubliclist")]
+        public IEnumerable<string> PublicList()
+        {
+            List<string> list = new List<string>()
+            {
+                "Ayşe","Fatma","Hayriye"
+            };
+            return list;
+        }
+
+    }
+}
